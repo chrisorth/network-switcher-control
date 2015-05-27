fe75ced5fe299086d8fb0b3785e614bb9dcf7036
Chris Orth <chris.orth@gmail.com>
2015-05-27 19:06:24 -0400

    Initial Commit

---
+++ network-switcher-control/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace network_switcher_control
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.networkNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.networkContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.networkContextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // networkNotifyIcon
+            // 
+            this.networkNotifyIcon.ContextMenuStrip = this.networkContextMenu;
+            this.networkNotifyIcon.Text = "notifyIcon1";
+            this.networkNotifyIcon.Visible = true;
+            this.networkNotifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.networkNotifyIcon_MouseClick);
+            this.networkNotifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.networkNotifyIcon_MouseDoubleClick);
+            // 
+            // networkContextMenu
+            // 
+            this.networkContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.networkContextMenu.Name = "networkContextMenu";
+            this.networkContextMenu.Size = new System.Drawing.Size(93, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.networkContextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon networkNotifyIcon;
+        private System.Windows.Forms.ContextMenuStrip networkContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ network-switcher-control/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace network_switcher_control
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            networkNotifyIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+            networkNotifyIcon.Text = "Network Switcher Control";
+        }
+
+        #region MainForm Handlers
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            this.ShowInTaskbar = false;
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                this.Visible = false;
+                e.Cancel = true;
+            }
+        }
+
+        #endregion
+
+        #region networkNotifyIcon Handlers
+
+        private void networkNotifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                networkContextMenu.Show();
+            }
+        }
+
+        private void networkNotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+        }
+
+        #endregion
+
+        #region exitToolStripMenuItem Handlers
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        #endregion
+
+        
+
+        
+    }
+}
